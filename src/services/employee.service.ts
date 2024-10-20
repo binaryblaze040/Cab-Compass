5764fa5fae757624ba98a662cb09c1e1f631a87c
Himanshu <66621256+binaryblaze040@users.noreply.github.com>
2024-10-20 15:58:29 +0530

    changes to fix build failure

--- src/services/employee.service.ts
+++ src/services/employee.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environment } from '../../environment';
+import { environment } from '../environments/environment';
 import { Employee } from '../types';
 import { Observable } from 'rxjs';
 
